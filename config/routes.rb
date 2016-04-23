0c68dfeb137d2221ad458e3e95113c741b8c36a5
ka <ka.kaosf@gmail.com>
2016-04-24 01:39:20 +0900

    Run "rails g controller home index"

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   resources :entries
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
