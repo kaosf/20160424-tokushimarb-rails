56c62b0c54ff744d78a2ce5c3d29b8f990730b3e
ka <ka.kaosf@gmail.com>
2016-04-24 01:07:35 +0900

    Add "null: false" to entries columns

--- db/migrate/20160423154910_create_entries.rb
+++ db/migrate/20160423154910_create_entries.rb
@@ -2,8 +2,8 @@ class CreateEntries < ActiveRecord::Migration
   def change
     create_table :entries do |t|
       t.references :user, index: true, foreign_key: true
-      t.string :title
-      t.string :body
+      t.string :title, null: false
+      t.string :body, null: false
       t.string :file
 
       t.timestamps null: false
