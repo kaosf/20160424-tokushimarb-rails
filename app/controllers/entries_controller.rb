164136557c3fd1befc6fd52470193e06b17a2e4b
ka <ka.kaosf@gmail.com>
2016-04-25 15:44:22 +0900

    Fix from "Entry.where ..." to "current_user.entries"

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -5,7 +5,7 @@ class EntriesController < ApplicationController
   # GET /entries
   # GET /entries.json
   def index
-    @entries = Entry.where(user: current_user)
+    @entries = current_user.entries
   end
 
   # GET /entries/1
